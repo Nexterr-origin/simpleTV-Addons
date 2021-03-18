1fcdc5248d2b5cbe9ae54d3874c82c6542871702
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-03-18 12:12:16 +0300

    Update +title-timeshift_ext.lua

--- timeshift-extensions/+title-timeshift_ext.lua
+++ timeshift-extensions/+title-timeshift_ext.lua
@@ -1,5 +1,6 @@
--- расширение дополнения httptimeshift - title (12/12/20)
--- Copyright © 2017-2020 Nexterr | https://github.com/Nexterr-origin/simpleTV-Addons 
+-- расширение дополнения httptimeshift - "title" (18/3/21)
+-- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Addons
+-- выводит на экране/панели 'Архив ...'
 	function httpTimeshift_title(eventType, eventParams)
 		if eventType == 'StartProcessing' then
 			if not eventParams.params
@@ -9,6 +10,7 @@
 			end
 			if eventParams.queryType == 'Start' then
 				if eventParams.params.offset > 0 then
+					m_simpleTV.Control.SetTitle('')
 					local timeshift_date = os.date('%x %X', (os.time() - math.floor(eventParams.params.offset / 1000)))
 					local month, day, year, hour, min, _ = timeshift_date:match('(%d+)/(%d+)/(%d+) (%d+):(%d+):(%d+)')
 					if day and month and hour and min then
@@ -17,10 +19,10 @@
 						month = tonumber(month)
 						local t = {'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря',}
 						month = t[month]
-						local str = ' (Архив ' .. day .. ' ' .. month .. ' в ' .. hour .. ':' .. min .. ')'
-						local title = m_simpleTV.Control.GetTitle()
-						title = title .. str
-						m_simpleTV.OSD.ShowMessageT({text = title, showTime = 1000 * 5, id = 'channelName'})
+						local str = string.format(' (Архив %s %s в %s:%s)', day, month, hour, min)
+						local title = m_simpleTV.Control.GetTitle() .. str
+						m_simpleTV.Control.SetTitle(title)
+						m_simpleTV.OSD.ShowMessageT({text = title, showTime = 5000, id = 'channelName'})
 					end
 				end
 			end
