700b4fcb466ce13df965390e805c6be99ba9d1d5
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2021-12-15 14:49:13 +0300

    Add files via upload

---
+++ timeshift-extensions/antifriz-timeshift_ext.lua
@@ -0,0 +1,33 @@
+-- расширение дополнения httptimeshift - antifriz (18/12/21)
+-- Copyright © 2017-2021 Nexterr | https://github.com/Nexterr-origin/simpleTV-Addons
+	function httpTimeshift_antifriz(eventType, eventParams)
+		if eventType == 'StartProcessing' then
+			if not eventParams.params
+				or not eventParams.params.address
+			then
+			 return
+			end
+			if not (eventParams.params.address:match('af%-stream.com')
+				and (eventParams.params.rawM3UString:match('tvg%-rec=')
+					or eventParams.params.rawM3UString:match('catchup%-days=')))
+			then
+			 return
+			end
+			if     eventParams.queryType == 'GetLengthByAddress'
+				or eventParams.queryType == 'TestAddress'
+				or eventParams.queryType == 'IsRecordAble'
+				or eventParams.queryType == 'Start'
+			then
+				eventParams.params.rawM3UString = 'catchup="shift" ' .. eventParams.params.rawM3UString
+				eventParams.params.rawM3UString = eventParams.params.rawM3UString:gsub('catchup%-source=".-"', '')
+			 return true
+			end
+			if eventParams.queryType == 'GetRecordAddress' then
+				eventParams.params.rawM3UString = 'catchup="default" ' .. eventParams.params.rawM3UString:gsub('catchup%-source=', 'catchup-record-source=')
+				eventParams.params.rawM3UString = eventParams.params.rawM3UString:gsub('{duration}', '{length}')
+			 return true
+			end
+		 return true
+		end
+	end
+	httpTimeshift.addEventExecutor('httpTimeshift_antifriz')
