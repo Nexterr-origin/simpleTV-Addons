38c5800c43939e2b24d0c779bc9c498bc6a0faee
Nexterr <80147948+Nexterr-origin@users.noreply.github.com>
2023-01-06 12:19:32 +0300

    Update limehd-timeshift_ext.lua

--- timeshift-extensions/limehd-timeshift_ext.lua
+++ timeshift-extensions/limehd-timeshift_ext.lua
@@ -1,4 +1,4 @@
--- расширение дополнения httptimeshift limeHD (5/1/23)
+-- расширение дополнения httptimeshift limeHD (6/1/23)
 -- Copyright © 2017-2023 Nexterr | https://github.com/Nexterr-origin/simpleTV-Addons
 	function httpTimeshift_limehd(eventType, eventParams)
 		if eventType == 'StartProcessing' then
@@ -10,8 +10,8 @@
 			if not ((eventParams.params.address:match('limehd%.')
 				or eventParams.params.address:match('iptv2022%.'))
 				and m_simpleTV.User
-				and m_simpleTV.User.infolink
-				and m_simpleTV.User.infolink.url_archive)
+				and m_simpleTV.User.limehd
+				and m_simpleTV.User.limehd.url_archive)
 			then
 			 return
 			end
@@ -21,7 +21,7 @@
 				if eventParams.params.offset > 0 then
 					local starttime = math.floor(os.time() - (eventParams.params.offset / 1000))
 					local len = os.time() - starttime
-					eventParams.params.address = m_simpleTV.User.infolink.url_archive .. '/index-' .. starttime .. '-' .. len .. '.m3u8'
+					eventParams.params.address = m_simpleTV.User.limehd.url_archive .. '/index-' .. starttime .. '-' .. len .. '.m3u8'
 				end
 			 return true
 			end
